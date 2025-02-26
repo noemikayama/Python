22e051a6cc9a826fe22bcbd8d02e1b3ff9fcb7e2
noemikayama <n_kayama@hotmail.com>
2025-02-26 17:44:19 -0300

    oi

--- Lab 2/Comandos print()/Exemplos print() format.py
+++ Lab 2/Comandos print()/Exemplos print() format.py
@@ -7,15 +7,16 @@ peso = 49.56345
 #####################################################################################
 
 # Sem espaços específicos e sem identação
-print("\n\n Nome: {} \t idade: {} anos peso: {} kg\n" .format(nome, idade, peso)) 
+print("\n\n Nome: {} \t Idade: {}anos \t\t Peso:{}kg\n" .format(nome, idade, peso)) 
 
 # 15 espaços para nome; 3 espaços para idade; duas casas decimais para peso
 
 # Sem alinhamento/identação
-print(" Nome: {:15} \tIdade: {:3} anos\tPeso:{:.2f}Kg\n" .format(nome, idade, peso)) 
+print(" Nome: {:15} \t Idade: {:3}anos \t Peso:{:.2f}kg\n" .format(nome, idade, peso)) 
 # Alinhado/identado para a esquerda
-print(" Nome: {:<15} \tIdade: {:<3}anos\tPeso:{:.2f}Kg\n" .format(nome, idade, peso)) 
+print(" Nome: {:<15} \t Idade: {:<3}anos \t Peso:{:.2f}kg\n" .format(nome, idade, peso)) 
 # Alinhado/identado para a direita
-print(" Nome: {:>15} \tIdade: {:>3}anos\tPeso:{:.2f}Kg\n" .format(nome, idade, peso)) 
+print(" Nome: {:>15} \t Idade: {:>3}anos \t Peso:{:.2f}kg\n" .format(nome, idade, peso)) 
 # Alinhado/identado no centro
-print(" Nome: {:^15} \tIdade: {:^3}anos\tPeso:{:.2f}Kg\n" .format(nome, idade, peso))
+print(" Nome: {:^15} \t Idade: {:^3}anos \t Peso:{:.2f}kg\n" .format(nome, idade, peso))
+

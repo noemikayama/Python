293b6bc712f7f40fa3441d20fda89babc728bf68
noemikayama <n_kayama@hotmail.com>
2025-02-26 17:59:23 -0300

    oi

--- Lab 2/Comandos print()/Exemplos print() f com reais.py
+++ Lab 2/Comandos print()/Exemplos print() f com reais.py
@@ -1,6 +1,11 @@
+# Exemplo usando método f -> Reais
+
 A=10/3
 B=333/7
 C=1978/11
+
+#########################################################################################
+
 print(f"\n\tSEM FORMATAÇÃO => Valor de A = {A}")
 print(f"\n\tSEM FORMATAÇÃO => Valor de B = {B}")
 print(f"\n\tSEM FORMATAÇÃO => Valor de C = {C}")
@@ -9,6 +14,7 @@ print(f"\n\t DUAS CASAS DECIMAIS => Valor de A = {A:8.2f}")
 print(f"\n\tNENHUMA CASA DECIMAL => Valor de B = {B:8.0f}")
 print(f"\n\t UMA CASA DECIMAL => Valor de C = {C:8.1f}")
 print("\n===============================================================\n")
+# Ler interação com o teclado
 input("\t\t\t\t\t\tTecle Algo Para Continuar....")
 print("\n===============================================================\n")
 print(f"\n\tTAMANHO '8' DUAS CASAS DECIMAIS => Valor de A = {A:8.2f}")

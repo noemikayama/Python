2f89c56087e94cf37307be7d1ab05f3b78598d40
noemikayama <n_kayama@hotmail.com>
2025-03-16 12:42:07 -0300

    io

--- Lab 4/Exemplos for/Exemplo 1 - lista.py
+++ Lab 4/Exemplos for/Exemplo 1 - lista.py
@@ -1,10 +1,6 @@
 # Mostrar valores x = 23, 45, 67, 49
 
-print("\n\n")       # Pular linhas
-
 for x in (23, 45, 67, 49):
     print(x, end=" ")       # Não pular linhas entre os números
-
-print("\n\n")       # Pular linhas
 
 

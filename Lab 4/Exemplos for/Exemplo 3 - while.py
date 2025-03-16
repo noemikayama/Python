6db5071304aa5039415e21d041e7867b78b5738f
noemikayama <n_kayama@hotmail.com>
2025-03-16 13:01:27 -0300

    oi

--- Lab 4/Exemplos for/Exemplo 3 - while.py
+++ Lab 4/Exemplos for/Exemplo 3 - while.py
@@ -1,3 +1,4 @@
+# Programa que pega n números inseridos, soma os pares e conta os ímpares
 
 n = int(input("\n\n Digite a quantidade de números a ser lida: "))
 contImpar = 0
@@ -11,7 +12,7 @@ while (cont < n):
     else:
         contImpar = contImpar + 1
     cont = cont + 1
-    
+
 print(f"\n\n\t A soma de pares lidos é {somaPar}")
 print(f"\n\t Foram lidos {contImpar} números ímpares \n\n")
 

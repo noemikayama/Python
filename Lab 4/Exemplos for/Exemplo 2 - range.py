c98b24e7454981cd95aeb32e80d338dd5bc22066
noemikayama <n_kayama@hotmail.com>
2025-03-16 12:54:27 -0300

    oi

--- Lab 4/Exemplos for/Exemplo 2 - range.py
+++ Lab 4/Exemplos for/Exemplo 2 - range.py
@@ -1,10 +1,6 @@
 # Mostrar valores do range
 
-print("\n\n")       # Pular linhas
-
 for a in range(10):
     print(a, end=" ")       # Não pular linhas entre os números
-
-print("\n\n")       # Pular linhas
 
 

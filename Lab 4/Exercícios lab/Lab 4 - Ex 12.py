5ae8e2140c837f6a20a2a926316ccb697f350694
noemikayama <n_kayama@hotmail.com>
2025-03-18 19:17:55 -0300

    oi

--- Lab 4/Exercícios lab/Lab 4 - Ex 12.py
+++ Lab 4/Exercícios lab/Lab 4 - Ex 12.py
@@ -3,9 +3,9 @@ prime = 0
 i = 1
 
 while (i <= num):
-    if (num % i == 0):
+    if (num % i == 0):          # If the remainder of the number typed by the counter is 0, then it can be divided by that number
         print(f"\n\n Can be divided by {i} \n\n")
-    elif (num % i != 0 and i != 1 and i != num):
+    elif (num % i != 0 and i != 1 and i != num):        #
         prime = prime + 1
     i = i + 1
 
